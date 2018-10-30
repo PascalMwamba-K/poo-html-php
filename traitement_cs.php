0e80c76ba13f3cd692d4169a1dd3a24a32bed623
Pascal Mwamba K <pascalmwambak@liviway.com>
2018-10-30 15:21:08 +0200

    création du fichier traiteent_cs.php

---
+++ traitement_cs.php
@@ -0,0 +1,10 @@
+<?php
+
+	if(isset($_POST['debut'], $_POST['concept_sujet'])){
+		$debut = $_POST['debut'];
+		$cs = $_POST["concept_sujet"];
+
+		publier_cs($debut,$cs);
+	}else{
+		echo "il y a un probleme dans les clés";
+	}
